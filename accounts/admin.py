ed0b1176d3bdf9aedaba53b5bdd9e368a3824c86
Temur <temuru502@gmail.com>
2025-04-01 11:45:43 +0500

    start app

---
+++ accounts/admin.py
@@ -0,0 +1,19 @@
+from  django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+from .models import CustomUser
+
+User=get_user_model()
+
+class UserAdmin(DjangoUserAdmin):
+    ordering=('pk',)
+    list_display=(
+        'email',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'is_active'
+    )
+
+admin.site.register(User,UserAdmin)
