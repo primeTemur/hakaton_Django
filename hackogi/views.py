92f4aaa73524690fa36ec2591693e1277f43f73a
Temur <temuru502@gmail.com>
2025-04-01 16:41:32 +0500

    hackaton home page

--- hackogi/views.py
+++ hackogi/views.py
@@ -1,4 +1,15 @@
+from django.shortcuts import render
+from django.utils import timezone
+from events.models import Event
 
-
-def home_page():
-    pass
+def home_page(request):
+    now=timezone.now()
+    active_hackathons=Event.objects.filter(
+        is_approved=True,
+        start_date__lt=now,
+        end_date__gt=now
+        )
+    
+    return render(request,'home.html',context={
+        'active_hackatons':active_hackathons
+    })
