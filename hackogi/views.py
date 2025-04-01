558fe307bc639720e0108c962e56d7e2cde3ebdb
Temur <temuru502@gmail.com>
2025-04-01 17:01:28 +0500

    hackothon in home page

--- hackogi/views.py
+++ hackogi/views.py
@@ -8,8 +8,19 @@ def home_page(request):
         is_approved=True,
         start_date__lt=now,
         end_date__gt=now
-        )
-    
+    )
+    past_hackatons=Event.objects.filter(
+        is_approved=True,
+        end_date__lt=now
+    )
+
+    upcoming_hackatons=Event.objects.filter(
+        is_approved=True,
+        start_date__gt=now
+    )
+
     return render(request,'home.html',context={
-        'active_hackatons':active_hackathons
+        'active_hackatons':active_hackathons,
+        'upcoming_hackatons':upcoming_hackatons,
+        'past_hackatons':past_hackatons,
     })
