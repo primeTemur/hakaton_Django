a2cdd31e54555dc3688cadbcda86a9b67ad72233
Temur <temuru502@gmail.com>
2025-04-02 09:47:12 +0500

    add login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from django.contrib.auth.forms import AuthenticationForm
+
+class CustomLoginForm(AuthenticationForm):
+    pass
+
+class LoginView(View):
+    def get(self,request):
+        form=AuthenticationForm()
+        return render(request,"accounts/login.html",context={
+            'form':form
+        })
+
--- hackogi/urls.py
+++ hackogi/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 from .views import home_page
 
 urlpatterns = [
+    
     path('',home_page),
+    path("accounts/",include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
