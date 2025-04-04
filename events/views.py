1401c112771f63b3abf1d069860f5e4106cbd3a1
Temur <temuru502@gmail.com>
2025-04-04 15:36:47 +0500

    the and

--- events/views.py
+++ events/views.py
@@ -3,7 +3,9 @@ from django.views import View
 from django.http.response import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, render
-from .models import Event,Idea
+from django.db.models import Exists,OuterRef
+
+from .models import Event,Idea,IdeaUpvote
 from .forms import CreateIdeaForm
 
 
@@ -15,10 +17,17 @@ class EventDetailView(View):
         except Event.DoesNotExist:
             return HttpResponse('Event does not exists')
         
+        ideas = event.ideas.annotate(
+            is_liked=Exists(
+                IdeaUpvote.objects.filter(user=request.user, idea=OuterRef("pk"))
+            )
+        )
         context={
             'event': event,
             'idea_form': CreateIdeaForm(initial={'event': event,'owner': request.user}),
+            'ideas': ideas,
         }
+
 
         return render(request,'events/event_detail.html',context=context)
 
@@ -40,3 +49,19 @@ class CreateEventIdeaView(LoginRequiredMixin,View):
             )
         
         return redirect("events:event-detail", event_id=event.id)
+    
+class UpvoteIdeaView(LoginRequiredMixin,View):
+    def post(self,request,event_id,idea_id):
+    
+        try:
+            idea=Idea.objects.get(pk=idea_id,event__id=event_id)
+        except Idea.DoesNotExist:
+            return HttpResponse('Idea does not exists')
+        
+        IdeaUpvote.objects.create(
+            user=request.user,
+            idea=idea
+        )
+
+        return redirect("events:event-detail", event_id=event_id)
+        
